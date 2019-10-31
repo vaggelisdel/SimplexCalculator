d5308efd6e35e4328e01af765cfe62daf0ea8743
vaggelisdel <delliosvaggelis@gmail.com>
2019-10-31 22:53:43 +0200

    JSON Restrictions

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Simplex = require('simplexcalculator');
 
-var minmax = "max";
+var minmax = "max";  //only min or max
 var objective = "50x1+120x2+40x3+80x4";
-var restrictions = ["-x1-2x1-3x2>=100", "x2+x3<=50"];
+var restrictions = ["1x+1y-1z>=100", "-8x-4y+z<=50", "-4y+2z>=27", "35x-22z<=52"];
 
 Simplex.Calculate(minmax, objective, restrictions, function (res) {
     console.log(res);
